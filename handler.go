71fe7955973b0ac0092f7651296239e0c4a1008e
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2021-03-24 15:36:33 +0900

    feat: impl handler

---
+++ handler.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+)
+
+func routing(r chi.Router) {
+	r.Get("/", handler)
+}
+
+func handler(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/plain")
+	time.Sleep(time.Millisecond * 500)
+	io.WriteString(w, "Limited")
+}
