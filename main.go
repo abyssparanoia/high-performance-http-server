209823f7ecf2b7d4b403b4c3b2e6e6b2c63664a9
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2021-03-24 15:36:50 +0900

    feat: impl server run

--- main.go
+++ main.go
@@ -1,8 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/pkg/errors"
+	"golang.org/x/net/netutil"
+)
 
 func main() {
 
-	fmt.Println("hello world")
+	var server *http.Server
+
+	defer func() {
+		err := recover()
+		if err != nil {
+			if e, ok := err.(error); ok {
+				err = errors.Wrapf(e, "panic occurred outside of the request processing")
+			}
+		}
+		gracefulShuttdown(server)
+	}()
+
+	addr := fmt.Sprintf(":%s", "8080")
+
+	listner, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+
+	r := chi.NewRouter()
+	routing(r)
+
+	// server
+	server = &http.Server{
+		Addr:         addr,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Handler:      r,
+	}
+
+	quit := make(chan os.Signal, 1)
+	go func() {
+		if err := server.Serve(netutil.LimitListener(listner, 1024)); err != http.ErrServerClosed {
+			log.Fatalf("[CLOSED] server closed with error")
+			quit <- syscall.SIGTERM
+		}
+	}()
+
+	log.Printf(fmt.Sprintf("[START] server. port: %s\n", addr))
+
+}
+
+func gracefulShuttdown(server *http.Server) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if server != nil {
+		err := server.Shutdown(ctx)
+		if err != nil {
+			log.Fatalf("failed to gracefully shutdown")
+		}
+	}
+
+	log.Fatalf("server shutdown completed")
 }
