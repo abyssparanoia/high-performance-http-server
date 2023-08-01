3fe35aa1beaab5a9fa10b587545352203cbb6525
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-08-01 10:12:32 +0900

    feat: add cancel catch middleware

--- client/main.go
+++ client/main.go
@@ -2,15 +2,48 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
+	"net"
 	"net/http"
+	"time"
 
 	"golang.org/x/sync/errgroup"
 )
 
 func main() {
 
+	singleRequest()
+
+}
+
+func singleRequest() {
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			DialContext: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).DialContext,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ResponseHeaderTimeout: 10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+		},
+		Timeout: 3 * time.Second,
+	}
+	resp, err := httpClient.Get("http://localhost:8080")
+	if err != nil {
+		fmt.Println("resp", resp)
+		panic(err)
+	}
+	defer resp.Body.Close()
+	byteArray, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(byteArray))
+}
+
+func bulkRequest() {
 	var eg errgroup.Group
 
 	for idx := 0; idx < 1050; idx++ {
@@ -21,7 +54,7 @@ func main() {
 				return err
 			}
 			defer resp.Body.Close()
-			byteArray, err := ioutil.ReadAll(resp.Body)
+			byteArray, err := io.ReadAll(resp.Body)
 			if err != nil {
 				return err
 			}
@@ -33,5 +66,4 @@ func main() {
 	if err := eg.Wait(); err != nil {
 		panic(err)
 	}
-
 }
